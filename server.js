a794dbf60d524e6e85e4df6ffadfd40a70620ceb
Joshua Gonzalez <jalgonz96@gmail.com>
2021-08-26 17:26:07 -0400

    connection string uri and options for mongoose

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/fitness";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+});
+
 app.listen(PORT,function(){
     console.log(`Now listening on Port ${PORT}`);
 }); 
