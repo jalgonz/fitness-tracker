ac25e8933ba2f997ba650b405c77c9c71406cf3d
Joshua Gonzalez <jalgonz96@gmail.com>
2021-08-26 18:40:10 -0400

    requiring morgan depenecy

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(morgan("dev"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/fitness";
 mongoose.connect(MONGODB_URI, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
-    useFindAndModify:false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 app.use(require("./routes/apiRoutes.js"));
